b8791c0a2e072057c04ec13a484e192f688db350
SANCHIT JAIN <sanchit@cse.iitb.ac.in>
2019-02-19 17:31:27 +0530

    check_similarity

--- Assignment3/checkSimilarity.py
+++ Assignment3/checkSimilarity.py
@@ -1,6 +1,7 @@
 import os
 
 import torch
+import torchfile
 
 sample_dir='./sample_output/'
 my_dir='./output/'
@@ -10,6 +11,22 @@ sample_list=['output','gradB','gradW']
 my_list=['output','gradB','gradWeight']
 
 
-for l in my_list:
-    print(l)
-    # out=
+for l in range(len(my_list)):
+    print(my_list[l])
+    print(sample_list[l])
+    sample_out=torchfile.load(sample_dir+sample_list[l]+sample_sufix+'.bin')
+    my_out=torch.load(my_dir+my_list[l]+'.bin')
+    # print(sample_out)
+    # print(my_out)
+    if l==0:
+        sample_out=torch.tensor(sample_out)
+        print(torch.sum((sample_out-my_out)**2))
+    else:
+        for m in range(len(my_out)):
+            sample_1=torch.tensor(sample_out[m])
+            my_1=my_out[m]
+            print(torch.sum((sample_1-my_1)**2))
+
+
+
+
