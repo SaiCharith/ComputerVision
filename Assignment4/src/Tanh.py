2f7ebb0b6c42e38132e46e9bcd3af95eca791869
MAYANK SINGHAL <mayanksinghal308@gmail.com>
2019-03-22 22:28:20 +0530

    Added tanh Layer

---
+++ Assignment4/src/Tanh.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+dtype = torch.double
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Tanh:
+    def __init__(self):
+        self.output = None
+        self.gradInput = None
+        self.layerName = 'tanh'
+        self.temp = None
+
+    def forward(self, input, isTrain=False):
+        self.output = input
+        self.temp = torch.exp(2.0*input)
+        self.output = (self.temp - 1.0)/(self.temp + 1.0)
+        return self.output
+        # print("Tanh Layer Forward")
+
+    def backward(self, input, gradOutput):
+        self.gradInput = gradOutput
+        self.gradInput *= (4.0*self.temp)/(self.temp + 1.0)**2.0
+        # print("Tanh Layer backward")
+        return self.gradInput
+
+    def clearGradParam(self):
+        return
+
+    def dispGradParam(self):
+        print("Tanh Layer")
+
+    def updateParam(self, learningRate, alpha, regularizer=0):
+        # print("Tanh Layer Update Weights & Biases: ")
+        return
