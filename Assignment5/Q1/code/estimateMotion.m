ac55e0846abc731b6d5a3a417e05ae54ba252c52
MAYANK SINGHAL <mayanksinghal308@gmail.com>
2019-04-20 01:20:38 +0530

    Location Issue Handled

--- Assignment5/Q1/code/estimateMotion.m
+++ Assignment5/Q1/code/estimateMotion.m
@@ -1,14 +1,14 @@
 function p = estimateMotion(frame1, frame2, point, TEMPLATE_SIZE, Ix, Iy)
-    pointy = point(1);
-    pointx = point(2);
+    pointx = point(1);
+    pointy = point(2);
 
     num_points = (2*TEMPLATE_SIZE+1)*(2*TEMPLATE_SIZE+1);
     coords = zeros(3, num_points);
     
     for i=1:(2*TEMPLATE_SIZE+1)
-            coords(1,(i-1)*(2*TEMPLATE_SIZE+1)+1:i*(2*TEMPLATE_SIZE+1)) = pointx-TEMPLATE_SIZE+i-1;
-            coords(2,(i-1)*(2*TEMPLATE_SIZE+1)+1:i*(2*TEMPLATE_SIZE+1)) = pointy-TEMPLATE_SIZE:pointy+TEMPLATE_SIZE;
-            coords(3,(i-1)*(2*TEMPLATE_SIZE+1)+1:i*(2*TEMPLATE_SIZE+1)) = 1;
+        coords(1,(i-1)*(2*TEMPLATE_SIZE+1)+1:i*(2*TEMPLATE_SIZE+1)) = pointx-TEMPLATE_SIZE+i-1;
+        coords(2,(i-1)*(2*TEMPLATE_SIZE+1)+1:i*(2*TEMPLATE_SIZE+1)) = pointy-TEMPLATE_SIZE:pointy+TEMPLATE_SIZE;
+        coords(3,(i-1)*(2*TEMPLATE_SIZE+1)+1:i*(2*TEMPLATE_SIZE+1)) = 1;
     end
 
     dwdp = zeros(num_points, 2, 6);
@@ -24,8 +24,8 @@ function p = estimateMotion(frame1, frame2, point, TEMPLATE_SIZE, Ix, Iy)
         H = H + dIdwdp(i, :)'*dIdwdp(i, :);
     end
     template = getPatch(frame1, coords);
-    p = rand(2, 3);  % Initial Guess
-    for i=1:2000 %TODO for better convergenece
+    p = zeros(2, 3);  % Initial Guess
+    for i=1:20 %TODO for better convergenece
        warp = getPatch(frame2, ceil(p*coords));
        delP = ((template-warp)*dIdwdp/H)';
        p = p + reshape(delP, 3, 2)';
@@ -35,8 +35,4 @@ end
 function patch = getPatch(I, coords)
     ind = sub2ind(size(I), max(min(coords(1,:), size(I,1)), 1), max(min(coords(2,:), size(I,2)), 1));
     patch = I(ind);
-%     patch = zeros(size(coords, 2), 1);
-%     for i=1:size(coords, 2):
-%         patch(i) = I(coords(1,i), coords(2,i)); 
-%     end
 end
