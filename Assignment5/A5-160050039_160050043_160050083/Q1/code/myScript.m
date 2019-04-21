87b6e828b5dd2cb188e4a3709e0f3f7ce6693be5
CHINTHAREDDY SAI CHARITH REDDY <charith@cse.iitb.ac.in>
2019-04-21 11:15:17 +0530

    Q2 done

---
+++ Assignment5/A5-160050039_160050043_160050083/Q1/code/myScript.m
@@ -0,0 +1,72 @@
+clear;
+close all;
+clc;
+
+%% Your Code Here
+% 1a)
+FIRST_FRAME = 1;
+I = imread(strcat("../input/", num2str(FIRST_FRAME), ".jpg"));
+featurePoints = detectSURFFeatures(I);
+figure;
+imshow(I); hold on;
+plot(featurePoints);
+
+% 1b)
+% Image gradients of first frame using sobel operator
+TEMPLATE_SIZE = 3; % 7 X 7 Window
+NUM_OF_POINTS = 4;
+salientFeaturePoints = getFeaturePoints(I, NUM_OF_POINTS, TEMPLATE_SIZE);
+figure;
+imshow(I); hold on;
+plot(salientFeaturePoints);
+pause(1);
+
+% REST
+NUM_OF_FRAMES = 39;
+templateFrame = double(imread(strcat("../input/", num2str(FIRST_FRAME), ".jpg")));
+
+allLocations = zeros(NUM_OF_FRAMES*NUM_OF_POINTS, 2);
+allLocations((FIRST_FRAME-1)*NUM_OF_POINTS+1:FIRST_FRAME*NUM_OF_POINTS, :) = salientFeaturePoints.Location;
+figure; imshow(uint8(templateFrame)); hold on;  plot(salientFeaturePoints.Location(:,1), salientFeaturePoints.Location(:,2), '*');
+[status, msg, msgID] = mkdir('ourOutput');  saveas(gcf, strcat("../ourOutput/", num2str(FIRST_FRAME), ".jpg"));
+
+newLocation = salientFeaturePoints.Location;
+for t=FIRST_FRAME+1:NUM_OF_FRAMES
+    if mod(t,5) == 0
+        templateFrame = double(imread(strcat("../input/", num2str(t-1), ".jpg")));
+        salientFeatures.Location = newLocation;
+    end
+    frame = double(imread(strcat("../input/", num2str(t), ".jpg")));
+    [Ix, Iy] = imgradientxy(imgaussfilt(frame, 1.0));
+    for k=1:NUM_OF_POINTS
+        point = round(salientFeaturePoints.Location(k,:));
+        point = [point(2) point(1)];
+        motion = estimateMotion(templateFrame, frame, point, TEMPLATE_SIZE, Ix, Iy);
+        newLocation(k, :) = motion*[point'; 1];
+    end
+    newLocation = [newLocation(:,2) newLocation(:,1)]; % swapping for plotting
+    allLocations((t-1)*NUM_OF_POINTS+1:t*NUM_OF_POINTS, :) = newLocation;
+    figure; imshow(uint8(frame)); hold on;  
+    plot(allLocations(:, 1), allLocations(:, 2), '*');
+    saveas(gcf, strcat("../ourOutput/", num2str(t), ".jpg"));
+end
+close all;
+% load gong.mat;  sound(y);
+%% Save all the trajectories frame by frame
+% variable trackedPoints assumes that you have an array of size 
+% No of frames * 2(x, y) * No Of Features
+% noOfFeatures is the number of features you are tracking
+% Frames is array of all the frames(assumes grayscale)
+% noOfPoints = 1;
+% for i=1:N
+%     NextFrame = Frames(i,:,:);
+%     imshow(uint8(NextFrame)); hold on;
+%     for nF = 1:noOfFeatures
+%         plot(trackedPoints(1:noOfPoints, 1, nF), trackedPoints(1:noOfPoints, 2, nF),'*')
+%     end
+%     hold off;
+%     saveas(gcf,strcat('output/',num2str(i),'.jpg'));
+%     close all;
+%     noOfPoints = noOfPoints + 1;
+% end 
+%    
---
+++ Assignment5/A5-160050039_160050043_160050083/Q1/newCode/myScript.m
@@ -0,0 +1,77 @@
+clear;
+close all;
+clc;
+%% Your Code Here
+load gong.mat;
+[status, msg, msgID] = mkdir('../ourOutput');  % Because output images get saved in ../ourOutput
+
+NUM_OF_FRAMES = 59;  % It starts reading from frame 1 till NUM_OF_FRAMES
+TEMPLATE_SIZE = 40;  % (2*3+1) X (2*3+1) = 7 X 7 Window
+NUM_OF_POINTS = 8;  % Number of salient features to extract
+
+I = imread(strcat("../input/", num2str(1), ".jpg"));
+featureType = @detectSURFFeatures;  % Type of features
+featurePoints = featureType(I);
+figure; imshow(I); hold on; plot(featurePoints.selectStrongest(NUM_OF_POINTS));
+saveas(gcf, "../ourOutput/All.jpg");
+
+salientFeaturePoints = getSalientFeaturePoints(featureType, I, NUM_OF_POINTS, TEMPLATE_SIZE);
+myplot(salientFeaturePoints.Location(:,1), salientFeaturePoints.Location(:,2), I, 0, NUM_OF_POINTS);   % View the salientfeaturepoints returned by the function
+
+templateFrame = double(I);
+
+trackedLocations = salientFeaturePoints.Location;   % It stores locations of feature points for all frames so far
+myplot(salientFeaturePoints.Location(:,1), salientFeaturePoints.Location(:,2), templateFrame, 1, NUM_OF_POINTS);   % View the first frame
+
+for t=2:NUM_OF_FRAMES
+    if mod(t,8) == 0
+        %   Change the template frame and the location of salient features
+        sizeTracked = size(trackedLocations, 1);
+        salientFeaturePoints.Location = trackedLocations(sizeTracked-NUM_OF_POINTS+1:sizeTracked, :);
+        templateFrame = double(imread(strcat("../input/", num2str(t-1), ".jpg")));
+    end
+    frame = double(imread(strcat("../input/", num2str(t), ".jpg")));
+    [Ix, Iy] = imgradientxy(frame);
+    threshold = TEMPLATE_SIZE*25;
+    count = 0;
+    if NUM_OF_POINTS == 0
+        break;
+        sound(y);
+    end
+    for k=1:NUM_OF_POINTS
+        point = salientFeaturePoints.Location(k-count,:);
+        point = [point(2), point(1)];   % Checking with swapping
+        [motion, L2error] = estimateMotion(templateFrame, frame, point, TEMPLATE_SIZE, Ix, Iy);   % TODO try (nice)swapping point here and (bad)smoothing
+        point = [point(2), point(1)];   % Checking with swapping
+        newLocation = motion*[point'; 1];
+        %   Removing bad points, If L2 error is more than threshold than discard that point
+        %   Discarding includes changing salientfeatures and reducing num of points
+        if L2error > threshold || newLocation(1) < 0 || newLocation(2) < 0 || L2error == 0
+            fprintf("Rejected ");
+            salientFeaturePoints = [salientFeaturePoints(1:k-count-1); salientFeaturePoints(k-count+1:NUM_OF_POINTS-count)];
+            count = count + 1;
+        else
+            trackedLocations = [trackedLocations; newLocation'];
+        end
+        fprintf("L2 Error for frame no. %d, & point no. %d, is = %d\n", t, k, L2error);
+    end
+    NUM_OF_POINTS = NUM_OF_POINTS - count;
+    sizeTracked = size(trackedLocations, 1);
+    myplot(trackedLocations(sizeTracked-NUM_OF_POINTS+1:sizeTracked, 1), trackedLocations(sizeTracked-NUM_OF_POINTS+1:sizeTracked, 2), frame, t, NUM_OF_POINTS);
+%     myplot(trackedLocations(:, 1), trackedLocations(:, 2), frame, t, NUM_OF_POINTS);
+end
+close all;
+sound(y);
+
+function [] = myplot(xCoords, yCoords, image, index, np)
+    % Caller should not swap points as required for plot
+    % Assuming four different type of points are there    
+    % xCoords and yCoords should be column vectors    
+    figure; imshow(uint8(image)); hold on;
+    s = size(yCoords, 1);
+    colors = ["c*", "y*", "m*", "g*", "b*", "r*", "k*", "w*"];
+    for i=1:np
+        plot(yCoords(i:np:s), xCoords(i:np:s), colors(mod((i-1), 8) + 1), 'MarkerSize', 8, 'LineWidth', 1);
+    end
+    saveas(gcf, strcat("../ourOutput/", num2str(index), ".jpg"));
+end
